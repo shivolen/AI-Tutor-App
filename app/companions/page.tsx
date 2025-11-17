19c947d71cc6b690b097112cd08ab00412df797c
Shivolen <mshivolen@gmail.com>
2025-11-17 10:55:14 +0530

    implemented companion library

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,6 +1,33 @@
-const page = () => {
+import CompanionCard from "@/components/CompanionCard";
+import { getAllCompanions } from "@/lib/actions/companion.actions";
+import { subjectsColors } from "@/constants";
+
+const getSubjectColor = (subject: string) => {
+  return subjectsColors[subject.toLowerCase() as keyof typeof subjectsColors] || "#E5E7EB"; // default gray
+};
+
+const page = async ({searchParams}: SearchParams) => {
+  const filters = await searchParams;
+  const subject = filters.subject ? filters.subject : '';
+  const topic = filters.topic ? filters.topic : '';
+  const companions = await getAllCompanions({subject,topic});
+
+  console.log(companions);
+
   return (
-    <div>Companions Library</div>
+    <main>
+      <section className='flex justify-between gap-4 max-sm:flex-col'>
+        <h1>Companion Library</h1>
+        <div className='flex gap-4'>Filters</div>
+      </section>
+      <section className='companions-grid'>
+        {companions.map((companion) =>(
+          <CompanionCard key={companion.id}
+           {... companion}
+           color={getSubjectColor(companion.subject)} />
+        ))}
+      </section>
+    </main>
   )
 }
 
--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -16,3 +16,24 @@ export const createCompanion = async (formData: CreateCompanion) => {
 
     return data[0];
 }
+
+export const getAllCompanions = async ({limit =10, page =1, subject, topic}:GetAllCompanions) => {
+    const supabase = createSupabaseClient();
+
+    let query = supabase.from('Companions').select();
+
+    if (subject && topic) {
+        query = query.ilike('subject', `%${subject}%`)
+        .or(`topic.ilike.%${topic}%,name.ilike.%${topic}%`)
+      } else if(subject) {
+        query = query.ilike('subject', `%${subject}%`);
+      } else if(topic) {
+        query = query.or(`topic.ilike.%${topic}%,name.ilike.%${topic}%`)
+    }
+
+    const { data, error } = await query.range((page - 1) * limit, page * limit - 1);
+
+    if(error) throw new Error(error.message);
+
+    return data;
+}
